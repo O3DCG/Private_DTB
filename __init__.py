0fd2ad2e8b2182f5bc13e62569c19b91dc3c1af3
unknown <oumi.3dcg@gmail.com>
2020-09-26 21:14:43 +0900

    modified for new SkinShader

--- __init__.py
+++ __init__.py
@@ -291,6 +291,8 @@ class IMP_OT_FBX(bpy.types.Operator):
                 drb.integrationEyelashes()
                 Global.deselect()
             ds.makeDct()
+            DtbMaterial.McyFresnel()
+            DtbMaterial.McyMicro()
             DtbMaterial.McySkin()
             DtbMaterial.McyEyeWet()
             DtbMaterial.McyEyeDry()
